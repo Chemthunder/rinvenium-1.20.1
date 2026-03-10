f8a3526d5c4aa6d1eab0a2f5a03520c8530b009f
Midget807 <phu7mai@gmail.com>
2026-03-10 12:55:14 +1030

    balls

---
+++ src/main/java/silly/chemthunder/rinvenium/index/RinveniumArmorMaterials.java
@@ -0,0 +1,11 @@
+package silly.chemthunder.rinvenium.index;
+
+import silly.chemthunder.rinvenium.Rinvenium;
+
+public class RinveniumArmorMaterials {
+
+
+    public static void registerModArmorMaterials() {
+        Rinvenium.LOGGER.info("Registering Rinvenium Armor Materials");
+    }
+}
---
+++ src/main/java/silly/chemthunder/rinvenium/item/EnvixiaArmorItems.java
@@ -0,0 +1,10 @@
+package silly.chemthunder.rinvenium.item;
+
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+
+public class EnvixiaArmorItems extends ArmorItem {
+    public EnvixiaArmorItems(ArmorMaterial material, Type type, Settings settings) {
+        super(material, type, settings);
+    }
+}
