16bd7532406052cbcc0c5ea53378bbf11d2b076f
Midget807 <phu7mai@gmail.com>
2026-03-30 00:56:25 +1030

    regen fix

--- src/main/java/silly/chemthunder/rinvenium/index/RinveniumFoodComponents.java
+++ src/main/java/silly/chemthunder/rinvenium/index/RinveniumFoodComponents.java
@@ -7,7 +7,7 @@ import silly.chemthunder.rinvenium.Rinvenium;
 
 public class RinveniumFoodComponents {
     public static final FoodComponent BATTERY = new FoodComponent.Builder().hunger(4).saturationModifier(0.6f).build();
-    public static final FoodComponent ION_CELL = new FoodComponent.Builder().hunger(8).saturationModifier(1.2f).statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 8, 0), 1.0f).alwaysEdible().build();
+    public static final FoodComponent ION_CELL = new FoodComponent.Builder().hunger(8).saturationModifier(1.2f).statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 140, 0), 1.0f).alwaysEdible().build();
 
     public static void initRinveniumFoodComponents() {
         Rinvenium.LOGGER.info("Initializing Rinvenium Food Components");
