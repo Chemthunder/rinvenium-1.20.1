bdc1ac7074b91d3f9ae112b07a688ea35196ffe7
Midget807 <phu7mai@gmail.com>
2026-05-18 16:54:02 +0930

    Death message recolor

---
+++ src/main/java/silly/chemthunder/rinvenium/mixin/ServerPlayerEntityMixin.java
@@ -0,0 +1,32 @@
+package silly.chemthunder.rinvenium.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.mojang.authlib.GameProfile;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageTracker;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import silly.chemthunder.rinvenium.index.RinveniumDamageSources;
+
+@Mixin(ServerPlayerEntity.class)
+public abstract class ServerPlayerEntityMixin extends PlayerEntity {
+    public ServerPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
+        super(world, pos, yaw, gameProfile);
+    }
+
+    @WrapOperation(method = "onDeath", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/damage/DamageTracker;getDeathMessage()Lnet/minecraft/text/Text;"))
+    private Text rinvenium$customDeathMessageColor(DamageTracker instance, Operation<Text> original, @Local(argsOnly = true) DamageSource damageSource) {
+        if (damageSource.isOf(RinveniumDamageSources.ORCHID)) {
+            return original.call(instance).copy().formatted(Formatting.YELLOW);
+        }
+        return original.call(instance);
+    }
+}
